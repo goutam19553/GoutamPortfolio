f27e9d72ee9e0bc8b16b1d3299d25bb13ec83f2a
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-08-31 18:46:09 +0530

    Update animation-lottie.jsx

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+// Dynamically import Lottie so it only loads on the client
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
+const AnimationLottie = ({ animationPath, width = "95%" }) => {
   return (
-    <Lottie {...defaultOptions} />
+    <Lottie
+      animationData={animationPath}
+      loop
+      autoplay
+      style={{ width }}
+    />
   );
 };
 

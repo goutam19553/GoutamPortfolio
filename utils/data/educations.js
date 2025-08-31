74414c1af59030c462da4f607bd796b61707b74e
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-08-31 14:40:17 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Self-Taught Professional",
+    duration: "2018 - Present",
+    institution: "Independent Learning (Technology, Marketing, AI, Web Development, Freelance Projects)",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2016 - 2018",
+    institution: "SJPN Trust PU College",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2006 - 2016",
+    institution: "Shri Saraswati School",
   }
 ]

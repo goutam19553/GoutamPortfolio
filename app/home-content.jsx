aba36720a09bc669f847f1a27e2897329a1c7290
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-08-31 19:27:06 +0530

    Create home-content.jsx

---
+++ app/home-content.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+// Dynamic imports with SSR disabled for DOM-dependent components
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const Blog = dynamic(() => import("./components/homepage/blog"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
+
+export default function HomeContent({ blogs }) {
+  return (
+    <div suppressHydrationWarning>
+      <HeroSection />
+      <AboutSection />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Education />
+      <Blog blogs={blogs} />
+      <ContactSection />
+    </div>
+  );
+}

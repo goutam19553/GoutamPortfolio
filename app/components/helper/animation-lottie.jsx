6f39cc054966453750fe13aa60302d55d5b63745
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-08-31 18:47:03 +0530

    Update animation-lottie.jsx

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,11 +1,17 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-// Dynamically import Lottie so it only loads on the client
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
 
 const AnimationLottie = ({ animationPath, width = "95%" }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null; // Prevents hydration mismatch
+
   return (
     <Lottie
       animationData={animationPath}

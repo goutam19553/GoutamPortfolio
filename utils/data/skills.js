c62f428164dce8ef05041a4e592c0632a81143c2
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-08-31 18:11:15 +0530

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,11 +1,10 @@
 export const skillsData = [
-  // Development & Programming
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
+  'Javascript',
+  'Typescript',
   'React',
-  'Next.js',
+  'Next JS',
   'Tailwind',
   'MongoDB',
   'MySQL',
@@ -19,21 +18,93 @@ export const skillsData = [
   'Firebase',
   'MaterialUI',
   'Nginx',
-  'Strapi',
-
-  // AI / AR / Emerging Tech
-  'Artificial Intelligence (AI)',
-  'Augmented Reality (AR)',
-  'Machine Learning Basics',
-
-  // Marketing & Strategy
-  'SEO',
-  'Digital Marketing',
-  'Content Strategy',
-  'Campaign Management',
-  'Branding',
-
-  // Writing & Communication
-  'Ghostwriting',
-
+  'Strapi'
 ]
+
+// Choose your skills from below. Make sure it's in the same format and spelled correctly.
+// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+
+
+// AVAILABLE SKILLS
+
+/* 
+  HTML
+  CSS
+  JS 
+  React
+  Next JS
+  Nuxt JS
+  Node JS
+  Vue
+  Angular
+  Docker
+  Photoshop
+  Illustrator
+  Svelte
+  GCP
+  Azure
+  Fastify
+  Haxe
+  Ionic
+  Markdown
+  Microsoft Office
+  Picsart
+  Sketch
+  Unity
+  WolframAlpha
+  Adobe XD
+  After Effects
+  Bootstrap
+  Bulma
+  CapacitorJs
+  Coffeescript
+  MemSQL
+  C
+  C++
+  C#
+  Python
+  Java
+  Julia
+  Matlab
+  Swift
+  Ruby
+  Kotlin
+  Go
+  PHP
+  Flutter
+  Dart
+  Typescript
+  Swift
+  Git
+  Figma
+  Canva
+  Ubuntu
+  Bootstrap
+  MongoDB
+  Tailwind
+  ViteJS
+  VuetifyJS
+  MySQL
+  PostgreSQL
+  Prisma
+  AWS
+  Firebase
+  Blender
+  Premiere Pro
+  Adobe Audition
+  Deno
+  Django
+  Gimp
+  Graphql
+  Lightroom
+  MaterialUI
+  Nginx
+  Numpy
+  OpenCV
+  Pytorch
+  Selenium
+  Strapi
+  Tensorflow
+  Webex
+  Wordpress
+*/

e0de2588dba921301cfb9904753ce2afd32c9b9d
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-08-31 19:05:03 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,22 +2,29 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import Footer from "./components/footer";
 import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
+
 import "./css/card.scss";
 import "./css/globals.scss";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Portfolio of Goutam P - SEO-Centric Full Stack Developer",
   description:
-    "This is the portfolio of Goutam P. I’m a self-taught full stack developer and digital marketing specialist with strong expertise in SEO and AI-driven solutions. I love exploring new things, collaborating with others, and taking on fresh challenges as a quick learner.
+    "This is the portfolio of Goutam P. I’m a self-taught full stack developer and digital marketing specialist with strong expertise in SEO and AI-driven solutions. I love exploring new things, collaborating with others, and taking on fresh challenges as a quick learner.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM!} />
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
@@ -27,7 +34,6 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }

66e22618dcc05a721e779f2f3f5b22c6decd7b9b
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-08-31 17:15:45 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,18 @@
 export const personalData = {
   name: "GOUTAM P",
-  profile: '/profile.png',
+  profile: "/profile.png",
   designation: "SEO-Centric Full Stack Developer & Campaign Strategist",
-  description: "My name is GOUTAM P. I am a self-taught professional with experience in digital marketing, web development, AI/AR technologies, and content strategy. I have worked with companies, SMEs, government bodies, and political leaders—delivering campaigns, building websites, and ghostwriting books, articles, and impactful speeches. I am passionate about problem-solving, independent learning, and combining technology with communication to create meaningful solutions. Always eager to explore new ideas and take on opportunities that match my skills and interests."
-  email: 'goutampujeri06@gmail.com',
-  phone: '+918050853225',
-  address: 'Belagavi,Karnataka, India 590001 ',
-  github: 'https://github.com/',
-  facebook: 'https://www.facebook.com//',
-  linkedIn: 'https://www.linkedin.com/in/goutam-p-918237282/?originalSubdomain=in',
-  twitter: 'https://twitter.com/',
-  stackOverflow: 'https://stackoverflow.com/',
-  leetcode: "https://leetcode.com/
+  description:
+    "My name is GOUTAM P. I am a self-taught professional with experience in digital marketing, web development, AI/AR technologies, and content strategy. I have worked with companies, SMEs, government bodies, and political leaders—delivering campaigns, building websites, and ghostwriting books, articles, and impactful speeches. I am passionate about problem-solving, independent learning, and combining technology with communication to create meaningful solutions. Always eager to explore new ideas and take on opportunities that match my skills and interests.",
+  email: "goutampujeri06@gmail.com",
+  phone: "+918050853225",
+  address: "Belagavi, Karnataka, India 590001",
+  github: "https://github.com/",
+  facebook: "https://www.facebook.com/",
+  linkedIn: "https://www.linkedin.com/in/goutam-p-918237282/?originalSubdomain=in",
+  twitter: "https://twitter.com/",
+  stackOverflow: "https://stackoverflow.com/",
+  leetcode: "https://leetcode.com/",
   devUsername: "Goutamdev",
-  resume: "https://drive.google.com/file/d/1dhMmjCloppF645jsFhbpLeBdA8ECQoGI/view?usp=sharing"
-}
+  resume: "https://drive.google.com/file/d/1dhMmjCloppF645jsFhbpLeBdA8ECQoGI/view?usp=sharing",
+};
